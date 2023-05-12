440949e728acbdcf31ac86cadc56ce7f29de8958
DTPAaron <hrnaltun44@gmail.com>
2023-05-12 20:30:02 +0300

    Initial version

---
+++ src/practice/Ortalama.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Ortalama {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz : ");
+        mat=input.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik=input.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya=input.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce=input.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih=input.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik=input.nextInt();
+        double sonuc =  (mat+fizik+kimya+turkce+tarih+muzik)/6.0;
+        System.out.println("Ortalamanız : "+sonuc);
+    }
+}

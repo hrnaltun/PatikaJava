a1ccf1e400b9ac8bfaa7c8da84d9a02816c6c215
DTPAaron <hrnaltun44@gmail.com>
2023-05-13 17:48:30 +0300

    Initial version

---
+++ src/practice/KdvTutari.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.Scanner;
+
+public class KdvTutari {
+    public static void main(String[] args) {
+        double kdvli,kdvsiz,kdvtutari,kdvorani;
+        Scanner input=new Scanner(System.in);
+        kdvsiz= input.nextDouble();
+        kdvorani=(kdvsiz>1000)? 0.08:0.18;
+        kdvtutari=kdvsiz*kdvorani;
+        kdvli=kdvsiz+kdvsiz*kdvorani;
+        System.out.println("Kdv'siz "+kdvsiz);
+        System.out.println("Kdv'li " +kdvli);
+        System.out.println("Kdv tutarı "+kdvtutari);
+
+
+    }
+}

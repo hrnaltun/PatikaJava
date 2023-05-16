95e4b4c61ae9bbb060c7a9d798dbaf3c1cfb707b
DTPAaron <hrnaltun44@gmail.com>
2023-05-16 20:13:09 +0300

    Initial version

---
+++ src/practice/SayiSiralama.java
@@ -0,0 +1,38 @@
+package practice;
+
+import java.util.Scanner;
+
+public class SayiSiralama {
+    public static void main(String[] args) {
+        int n1,n2,n3;
+        Scanner input=new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz: ");
+        n1=input.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2=input.nextInt();
+
+        System.out.print("Üçüncü sayıyı giriniz: ");
+        n3=input.nextInt();
+
+        if ((n1<n2)&&(n1<n3)){
+            if (n2<n3){
+                System.out.println(n1+"<"+n2+"<"+n3);
+            }else{
+                System.out.println(n1+"<"+n3+"<"+n2);
+            }
+        } else if ((n2<n1)&&(n2<n3)) {
+            if (n1<n3){
+                System.out.println(n2+"<"+n1+"<"+n3);
+            }else{
+                System.out.println(n2+"<"+n3+"<"+n1);
+            }
+        }else if ((n3 < n1) && (n3 < n2)){
+            if (n2<n1){
+                System.out.println(n3+"<"+n2+"<"+n1);
+            }else{
+                System.out.println(n3+"<"+n1+"<"+n2);
+            }
+        }
+    }
+}

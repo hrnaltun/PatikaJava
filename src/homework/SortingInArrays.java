a7ed1283a6d357f1c19f19a275956d53807331fb
DTPAaron <hrnaltun44@gmail.com>
2023-06-30 10:34:48 +0300

    Initial version

---
+++ src/homework/SortingInArrays.java
@@ -0,0 +1,21 @@
+package homework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortingInArrays {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Dizi boyutunu giriniz: ");
+        int length=inp.nextInt();
+        int[] array=new int[length];
+
+        for (int i=0;i<length;i++){
+            System.out.println("Sayıyı giriniz: ");
+            array[i]=inp.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}

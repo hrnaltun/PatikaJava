a54b4491112d3dc06f0f12bdf9f2869ed846bedb
DTPAaron <hrnaltun44@gmail.com>
2023-07-03 20:19:05 +0300

    Initial version

---
+++ src/practice/Palindrome.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Palindrome {
+    static boolean isPalindrome(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("kayak"));
+    }
+}
